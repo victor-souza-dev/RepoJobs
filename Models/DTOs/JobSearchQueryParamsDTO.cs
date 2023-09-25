e2cac9283c97d6bd8c20ee9346e91285af0ee99d
KlowdyTMS <klowdyacrux@gmail.com>
2023-09-24 21:37:54 -0300

    refactor Linkedin Search

--- Models/DTOs/JobSearchQueryParamsDTO.cs
+++ Models/DTOs/JobSearchQueryParamsDTO.cs
@@ -2,7 +2,7 @@
 
 public class JobSearchQueryParamsDTO
 {
-    public string Keyword { get; set; } = "";
-    public int Page { get; set; } = 1;
-    public int PageSize { get; set; } = 10;
+    public string Keyword { get; private set; } = "";
+    public int Page { get; private set; } = 1;
+    public int PageSize { get; private set; } = 10;
 }
--- Services/LinkedInSearch.cs
+++ Services/LinkedInSearch.cs
@@ -3,81 +3,127 @@ using OpenQA.Selenium;
 using Webscrapping.Models;
 using Webscrapping.Services.Interfaces;
 using OpenQA.Selenium.Interactions;
-using static System.Net.Mime.MediaTypeNames;
 
-namespace Webscrapping.Services
+namespace Webscrapping.Services;
+
+public class LinkedInSearch : ISearch
 {
-    public class LinkedInSearch : ISearch
+    public async Task<List<JobListing>> Search(string keyword)
     {
-        public async Task<List<JobListing>> Search(string keyword)
+        var jobListings = new List<JobListing>();
+        var startIndex = 0;
+        int limit = 20;
+        int delay = 2000;
+
+        var options = new ChromeOptions();
+        options.PageLoadStrategy = PageLoadStrategy.None;
+        options.AddArguments("--headless");
+
+        IWebDriver driver = new ChromeDriver(options);
+
+        driver.Navigate().GoToUrl($"https://www.linkedin.com/jobs/search?keywords={keyword}&location=Brasil&f_TPR=r604800");
+
+        await Task.Delay(delay * 4);
+
+        Actions actions = new Actions(driver);
+
+        try
         {
-            IWebDriver driver = new ChromeDriver();
-
-            driver.Navigate().GoToUrl($"https://www.linkedin.com/jobs/search?keywords={keyword}&location=Brasil&f_TPR=r604800");
-
-            await Task.Delay(5000);
-
-            Actions actions = new Actions(driver);
-
-            var jobListings = new List<JobListing>();
-            var numberOfEndClicks = 0;
-
-            try
+            int i = 0;
+            
+            while (true)
             {
-                while (numberOfEndClicks < 5)
+                actions.SendKeys(Keys.End).Perform();
+                await Task.Delay(delay);
+
+                var newJobListings = ExtractJobListings(driver, startIndex);
+                startIndex = jobListings.Count;
+
+                var distinctNewListings = newJobListings.Except(jobListings, new JobListingComparer()).ToList();
+
+                jobListings.AddRange(distinctNewListings);
+
+                try
                 {
-                    actions.SendKeys(Keys.End).Perform();
-                    await Task.Delay(3000);
-
-
-                    numberOfEndClicks++;
+                    if (distinctNewListings.Count == 0)
+                    {
+                        var loadMoreButton = driver.FindElement(By.XPath("/html/body/div[1]/div/main/section[2]/button"));
+                        if (loadMoreButton != null)
+                        {
+                            loadMoreButton.Click();
+                            await Task.Delay(delay);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                } catch(Exception ex)
+                {
+                    break;
                 }
-            }
-            catch (Exception ex)
-            {
-            }
-            finally
-            {
-                var newJobListings = ExtractJobListings(driver);
-                jobListings.AddRange(newJobListings);
-                driver.Quit();
-            }
 
-            return jobListings;
+                if (i == limit) break;
+
+                i++;
+            }
         }
-
-
-        private List<JobListing> ExtractJobListings(IWebDriver driver)
+        catch (Exception ex)
         {
-            var jobListings = new List<JobListing>();
-
-            IList<IWebElement> titleXPath = driver.FindElements(By.XPath(".//h3[contains(@class, 'base-search-card__title')]"));
-            IList<IWebElement> companyXPath = driver.FindElements(By.XPath(".//h4[contains(@class, 'base-search-card__subtitle')]"));
-            IList<IWebElement> locationXPath = driver.FindElements(By.XPath(".//span[contains(@class, 'job-search-card__location')]"));
-            IList<IWebElement> descriptionXPath = driver.FindElements(By.XPath(".//div[contains(@class, 'job-search-card__benefits')]"));
-            IList<IWebElement> postedDataXPath = driver.FindElements(By.XPath(".//time[contains(@class, 'job-search-card__listdate')]"));
-            IList<IWebElement> applyUrlXPath = driver.FindElements(By.XPath(".//a[contains(@class, 'base-card__full-link absolute top-0 right-0 bottom-0 left-0 p-0 z-[2]')]"));
-
-            for (int i = 0; i < titleXPath.Count; i++)
-            {
-                string title = titleXPath[i].Text;
-                string company = companyXPath[i].Text;
-                string location = locationXPath[i].Text;
-                string postedData = postedDataXPath[i].Text;
-
-                jobListings.Add(new JobListing
-                (
-                    title,
-                    company,
-                    location,
-                    "",
-                    postedData,
-                    ""
-                ));
-            }
-
-
-            return jobListings;
+            
         }
+        finally
+        {
+            driver.Quit();
+        }
+
+        Console.Write(jobListings.Count);
+        return jobListings;
+    }
+
+    private List<JobListing> ExtractJobListings(IWebDriver driver, int startIndex)
+    {
+        var jobListings = new List<JobListing>();
+
+        IList<IWebElement> xPath = driver.FindElements(By.XPath("/html/body/div[1]/div/main/section[2]/ul/li"));
+
+        for (int i = startIndex; i < xPath.Count; i++)
+        {
+            string title = xPath[i].FindElement(By.XPath(".//h3[contains(@class, 'base-search-card__title')]")).Text;
+            string company = xPath[i].FindElement(By.XPath(".//h4[contains(@class, 'base-search-card__subtitle')]")).Text;
+            string location = xPath[i].FindElement(By.XPath(".//span[contains(@class, 'job-search-card__location')]")).Text;
+            string postedData = xPath[i].FindElement(By.XPath(".//time[contains(@class, 'job-search-card__listdate')]")).Text;
+            string applyUrl = xPath[i].FindElement(By.XPath("./div/a")).GetAttribute("href");
+
+            jobListings.Add(new JobListing
+            (
+                title,
+                company,
+                location,
+                "",
+                postedData,
+                applyUrl
+            ));
+        }
+
+        return jobListings;
+    }
+}
+
+public class JobListingComparer : IEqualityComparer<JobListing>
+{
+    public bool Equals(JobListing x, JobListing y)
+    {
+        if (x == null && y == null)
+            return true;
+        if (x == null || y == null)
+            return false;
+
+        return x.Title == y.Title && x.Company == y.Company && x.Location == y.Location;
+    }
+
+    public int GetHashCode(JobListing obj)
+    {
+        return obj.Title.GetHashCode() ^ obj.Company.GetHashCode() ^ obj.Location.GetHashCode();
     }
 }
